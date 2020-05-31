e67cda3f41ed003a73e16f3250911b55cf3f1012
outk <outk1023@gmail.com>
2020-06-01 01:57:48 +0900

    I succeeded making a container systemsudo git status

--- main.go
+++ main.go
@@ -11,22 +11,36 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("what??")
 	}
 }
 
 func run() {
-	fmt.Printf("running %v\n", os.Args[2:])
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	}
+
+	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("running %v as PID %d\n", os.Args[2:], os.Getpid())
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
-	}
 
+	must(syscall.Chroot("/home/rootfs"))
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
 	must(cmd.Run())
 }
 
